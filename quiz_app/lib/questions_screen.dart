989f6153f563a6aa0287c6e478b00fa717fa5888
Geoffrey <97751898+GeoffreyBB@users.noreply.github.com>
2026-03-15 19:54:48 +0200

    Add files via upload

---
+++ quiz_app/lib/questions_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    }
+  }
+
+  @override
+  Widget build(context) {
+    if (currentQuestionIndex >= questions.length) {
+      return const Center(
+        child: Text(
+          'Quiz Complete!',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      );
+    }
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text, // fixed from .text to .question
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ...currentQuestion.shuffledAnswers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+            color: const Color.fromARGB(
+              118,
+              255,
+              255,
+              255,
+            ), // shifiting the transparency of the image to make it look better on the background
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(fontSize: 22, color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: () {
+              startQuiz();
+            },
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt),
+            label: Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
